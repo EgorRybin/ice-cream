18f2d8b6624ff149b79b3b7a95588d6c1984c319
EgorRybin <antoniopareiro@gmail.com>
2022-09-14 12:02:00 +0300

    create

---
+++ src/mobile-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const menuBtnsRef = document.querySelectorAll('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileListRef = document.querySelector('.menu-mobil__list');
+
+  mobileListRef.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  menuBtnsRef.forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('is-open');
+
+      mobileMenuRef.classList.toggle('is-open');
+    });
+  });
+})();
